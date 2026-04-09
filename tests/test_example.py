7edba008172c798cdae24e372db669e34dc72233
Jason Smoliak <your-email@example.com>
2026-04-09 08:52:53 -0700

    Fix property access in example page UI test

--- tests/test_example.py
+++ tests/test_example.py
@@ -11,6 +11,6 @@ from test_data.ui_data import EXAMPLE_PAGE_DATA
 def test_open_example_page(driver, expected_heading, expected_url_fragment):
     page = ExamplePage(driver).load()
 
-    assert page.heading_text() == expected_heading
+    assert page.heading_text == expected_heading
     assert expected_url_fragment in driver.current_url.lower()
 
