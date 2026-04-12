ed7d5b527ad314cbb66809a940ed2ab7c7002e85
Jason Smoliak <your-email@example.com>
2026-04-12 08:18:12 -0700

    Refactor MoreInfoPage to use shared header component

--- pages/more_info_page.py
+++ pages/more_info_page.py
@@ -1,11 +1,12 @@
-from selenium.webdriver.common.by import By
-
+from components.header_component import HeaderComponent
 from pages.base_page import BasePage
 
 
 class MoreInfoPage(BasePage):
-    HEADING = (By.TAG_NAME, "h1")
+    def __init__(self, driver, timeout=10):
+        super().__init__(driver, timeout)
+        self.header = HeaderComponent(driver, timeout)
 
     @property
     def heading_text(self):
-        return self.get_text(self.HEADING)
+        return self.header.text
